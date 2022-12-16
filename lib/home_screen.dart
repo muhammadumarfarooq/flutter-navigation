08786369b50974f69eae8bd29b9164f894fb58cc
umar <umar@craveup.com>
2022-12-16 08:19:56 +0500

    side drawer

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,6 +14,41 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Navigation Drawer'),
         backgroundColor: const Color(0xff764abc),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text('Umar Farooq'),
+              accountEmail: Text('develop.umar@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Page 1'),
+              onTap: () {
+                Navigator.pushNamed(context, HomeScreen.id);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.calculate),
+              title: const Text('Page 2'),
+              onTap: () {
+                Navigator.pushNamed(context, ScreenTwo.id);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.circle_notifications),
+              title: const Text('Page 3'),
+              onTap: () {
+                Navigator.pushNamed(context, ScreenThree.id);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: [
           TextButton(
